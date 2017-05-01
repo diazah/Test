af1ef825179a122e28641b890f65f5bd0fd20c36
Nadia <nadia@nadia.ma>
2017-05-01 18:22:24 +0100

    to test in server

---
+++ R/HelloIEEER.R
@@ -0,0 +1,14 @@
+#' Hello IEEER
+#' 
+#' Basic hello world function to be called from the demo app
+#' 
+
+helloIEEER <-function(){
+library(IEEER)
+query <- list(au="Rabiner, L",pys=1970, pye=1970)
+count <- IEEE_count(query)
+library(jsonlite)
+reslt<-toJSON(count)
+return(reslt)
+}
+
